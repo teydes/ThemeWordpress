4d4336f79855fdae20fcfd9982d40c7249904753
Teydes <ytegueu@yahoo.fr>
2020-11-14 23:28:47 +0100

    Actions - les Hooks

--- wp-content/themes/montheme/functions.php
+++ wp-content/themes/montheme/functions.php
@@ -1,2 +1,18 @@
 <?php
-add_theme_support( 'title-tag' );
+
+
+function montheme_supports () {
+    add_theme_support('title-tag');
+}
+function montheme_register_assets () {
+    wp_register_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css');
+    wp_register_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js', ['popper', 'jquery'], false, true);
+    wp_register_script('popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js', [], false, true);
+    wp_deregister_script('jquery');
+    wp_register_script('jquery', 'https://code.jquery.com/jquery-3.5.1.slim.min.js', [], false, true);
+    wp_enqueue_style('bootstrap');
+    wp_enqueue_script('bootstrap');
+}
+
+add_action('after_setup_theme', 'montheme_supports');
+add_action('wp_enqueue_scripts', 'montheme_register_assets');
